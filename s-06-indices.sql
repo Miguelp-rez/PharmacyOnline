67f855fde012ddf1bc34e3f0944845fbe79a67c6
BettySanchez7 <31338064+BettySanchez7@users.noreply.github.com>
2019-06-05 19:01:49 -0500

    Agregagando avances

---
+++ s-06-indices.sql
@@ -0,0 +1,27 @@
+--@Autor(es):--Sánchez Díaz María Beatriz
+			 --Pérez Quiroz Miguel Angel
+--@Fecha creación: 02/06/2019
+--@Descripción:Script 6- índices
+
+
+prompt iniciando con usuario admin
+connect pesa_proy_admin 
+
+create unique index centro_operaciones_clave_iux on centro_operaciones(clave);
+
+create index empleado_nombre_ix on empleado(lower(nombre));
+create index farmacia_rfc_ix on farmacia(rfc);
+--CON FUNCIONES
+create index empleado_nombre_ix on empleado(lower(nombre));
+create index nombre_med_nombre_ix on nombre_medicamento(lower(nombre));
+create index cliente_nombre_ix on cliente(lower(nombre));
+--COMPUESTO
+create index empleado_rfc_fecha_ix on empleado(rfc, fecha_ingreso);
+--FK
+create index nombre_medicamento_med_id_ix on nombre_medicamento(medicamento_id);
+create index farmacia_med_presentacion_med_id_ix on farmacia_medicamento(presentacion_medicamento_id);
+
+
+
+
+
---
+++ status_pedido.sql
@@ -0,0 +1,8 @@
+
+
+--TABLA STATUS_PEDIDO
+insert into status_pedido(status_pedido_id, clave, descripcion) values(1,'CAPTURADO','pedido capturado');
+insert into status_pedido(status_pedido_id, clave, descripcion) values(2,'EN TRANSITO','pedido en transito');
+insert into status_pedido(status_pedido_id, clave, descripcion) values(3,'ENTREGADO','pedido entregado');
+insert into status_pedido(status_pedido_id, clave, descripcion) values(4,'DEVULTO','pedido devuelto');
+insert into status_pedido(status_pedido_id, clave, descripcion) values(5,'CANCELADO','pedido CANCELADO');
