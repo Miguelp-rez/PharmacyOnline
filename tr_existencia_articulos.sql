68f0f054afec3e188ab1e9e2f62fe64d03af6741
bettysanchez7 <bety.sanchez.diaz@gmail.com>
2019-06-06 22:29:17 -0500

    TR_INVENTARIO

---
+++ tr_existencia_articulos.sql
@@ -0,0 +1,48 @@
+set serveroutput on
+create or replace trigger tr_existencia_medicamentos
+  before insert 
+  or update of detalle_pedido_id
+  on detalle_pedido
+  for each row
+declare
+  v_unidades_disponibles farmacia_medicamento.unidades_disponibles%type;
+  v_medicamento_id farmacia_medicamento.medicamento_id%type;
+  v_centro_operaciones_id farmacia_medicamento.farmacia_id%type;
+  v_presentacion_medicamento_id farmacia_medicamento.presentacion_medicamento_id%type;
+
+begin
+   v_medicamento_id:=:new.medicamento_id;
+   v_centro_operaciones_id:=:new.centro_operaciones_id;
+   v_presentacion_medicamento_id:=:new.presentacion_medicamento_id;
+begin
+    --VERIFICANDO NUMERO DE UNIDADES ACTUALES
+   select unidades_disponibles into v_unidades_disponibles
+   from farmacia_medicamento fm
+   where farmacia_id= :new.centro_operaciones_id
+   and medicamento_id=:new.medicamento_id
+   and presentacion_medicamento_id=:new.presentacion_medicamento_id;
+   exception when no_data_found then
+   v_unidades_disponibles:=0;
+end;
+  --SI EL NUMERO DE UNIDADES ES MENOR A 20 YA NO DEJAR HACER LA INSERCIÓN
+   if v_unidades_disponibles<=20 then
+   raise_application_error(-20001,'Es necesario realizar un chequeo del inventario en
+      la farmacia con id '||
+      v_centro_operaciones_id ||
+      ' se estan quedando sin unidades disponibles del medicamento con id '||
+      v_medicamento_id
+      || 'en la presentacion con id ' ||
+      v_presentacion_medicamento_id||
+      ' NO ES POSIBLE REALIZAR OPERACION');
+   else 
+     update farmacia_medicamento set 
+     unidades_disponibles=unidades_disponibles-:new.unidades_medicamento
+     where farmacia_id= :new.centro_operaciones_id
+     and medicamento_id=:new.medicamento_id
+     and presentacion_medicamento_id=:new.presentacion_medicamento_id;
+    dbms_output.put_line('Actualizando inventario');
+  end if;
+
+end;
+/
+show errors
