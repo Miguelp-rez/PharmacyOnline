2856757c49c5a1b7ea88bad0c0320a6828b2bfe1
Miguel <miguel@pc-mpq.fi.unam>
2019-06-07 10:51:51 -0500

    Agrego tabla externa al main

--- main.sql
+++ main.sql
@@ -13,6 +13,7 @@ drop role rol_invitado;
 @@s-01-usuarios.sql
 @@s-02-entidades.sql
 @@s-03-tablas-temporales.sql
+@@s-04-tablas-externas.sql
 @@s-05-secuencias.sql
 @@s-06-indices.sql
 @@s-07-sinonimos.sql
